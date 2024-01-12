363b218ede428441b4dac8318f76c673bdb37347
codeaid-dev <codeaid.lab@gmail.com>
2024-01-12 14:27:07 +0900

    fix

--- htdocs/blog/index.php
+++ htdocs/blog/index.php
@@ -3,38 +3,35 @@ require_once 'config.php';
 
 $stmt = $pdo->query("SELECT id,title,created_at,updated_at FROM posts ORDER BY updated_at DESC");
 $posts = $stmt->fetchAll();
-?>
-<?php if (isset($_GET['post'])) { ?>
-  <?php
+if (isset($_GET['post'])) {
     $id = intval($_GET['post']);
     $stmt = $pdo->prepare("SELECT * FROM posts WHERE id=?");
     $stmt->bindParam(1, $id, PDO::PARAM_INT);
     $stmt->execute();
     $post = $stmt->fetch();
-  ?>
-  <?php if ($post) { ?>
-    <!DOCTYPE html>
-    <html lang="ja">
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>ブログアプリ</title>
-    </head>
-    <body>
-      <h1><?= htmlspecialchars($post['title']) ?></h1>
-      <p>
-        作成日時：<?php echo htmlspecialchars($post['created_at']); ?><br>
-        更新日時：<?php echo htmlspecialchars($post['updated_at']); ?>
-      </p>
-      <p><?php echo nl2br(htmlspecialchars($post['article'])); ?></p>
-      <p><a href="index.php">トップ</a></p>
-    </body>
-    </html>
-  <?php } else {
-    http_response_code(404);
-    include_once("404.html");
-    exit;
-  } ?>
+    if (!$post) {
+      http_response_code(404);
+      include_once("404.html");
+      exit;
+    }
+?>
+  <!DOCTYPE html>
+  <html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ブログアプリ</title>
+  </head>
+  <body>
+    <h1><?= htmlspecialchars($post['title']) ?></h1>
+    <p>
+      作成日時：<?php echo htmlspecialchars($post['created_at']); ?><br>
+      更新日時：<?php echo htmlspecialchars($post['updated_at']); ?>
+    </p>
+    <p><?php echo nl2br(htmlspecialchars($post['article'])); ?></p>
+    <p><a href="index.php">トップ</a></p>
+  </body>
+  </html>
 <?php } else { ?>
   <!DOCTYPE html>
   <html>
