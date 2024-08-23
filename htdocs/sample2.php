0c44f80b284a8b2fbe01f5737b59976513f5754c
codeaid-dev <codeaid.lab@gmail.com>
2024-08-23 14:56:04 +0900

    fix

--- htdocs/sample2.php
+++ htdocs/sample2.php
@@ -20,9 +20,9 @@
     $data = [[1,'Yamada',85],[2,'Tanaka',79],[3,'Suzuki',63]];
 
     try {
-      //$db = new PDO($dsn, $user, $password, $options); // MySQL
+      $db = new PDO($dsn, $user, $password, $options); // MySQL
       //$db = new PDO("sqlite:./db/sample.db", options:$options); // SQLite
-      $db = new PDO("sqlite:./db/sample.db"); // SQLite
+      //$db = new PDO("sqlite:./db/sample.db"); // SQLite
       print '<p>接続成功</p>';
 
       $db->exec("CREATE TABLE IF NOT EXISTS users(
